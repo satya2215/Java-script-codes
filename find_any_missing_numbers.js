c5ef42946bc83c566d09bfe81654aa650360be5c
Satya Prakash Panda <125157520+satya2215@users.noreply.github.com>
2024-11-04 17:53:44 +0000

    one new file added

---
+++ find_any_missing_numbers.js
@@ -0,0 +1,11 @@
+const input=[5,7,9,11,15,17,21]
+let a=[];
+for(let i=0;i<input.length-1;i++){
+  if(input[i+1]-input[i] !== 2){
+    a[i]=input[i]+2
+  }
+
+}
+a.forEach((a)=>{
+  document.write(a," ,")
+})
